d00a521f1e08f5cbf962184a50f2823a7015335f
XetrovTornado <puck.mcpuck@gmail.com>
2019-11-12 16:09:05 -1000

    scripts uploaded

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # CyberPatriots19-20
 Scripts for McK. HS CP19-20
+
+# Checklist
+
+Remember: use chmod a+x <file-name> before running so you have the permissions required
+
+limitsudo.sh
+
+```bash
+echo "test1';
+```
+
+passwd.sh
+
+```bash
+echo "test2";
+```
---
+++ pc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Changes all user's passwords that already have a password
+# Requires root login
+for username in $(grep -v '\:\!\:' /etc/shadow | grep -v '\:\*\:' | cut -d: -f1); do
+if [[ $username != "cyberpatriot" ]]; then
+echo "$username:FortniteMinecraft42!" | chpasswd;
+echo "Changed $username's password";
+fi;
+done
+
+
+
+
+
+
